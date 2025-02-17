313d43bb5015edafab6cfd069fa94d0b0cd4bd93
Matvey Dubrovin <mathewsun@rambler.ru>
2025-02-17 15:48:36 +0300

    new prj

---
+++ MassTransit_intro/Program.cs
@@ -0,0 +1,27 @@
+namespace MassTransit_intro
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
